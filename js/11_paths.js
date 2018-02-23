dd3635593dc3374adcfcf1caa0cb6c02e146d686
David Trussler <mail@davidtrussler.net>
2018-02-23 15:14:13 +0000

    Comment: Loads data from CSV file

--- js/11_paths.js
+++ js/11_paths.js
@@ -1,108 +1,16 @@
-var Barchart = function() {};
+var Paths = function() {};
 
-Barchart.prototype.draw = function() {
-	var _this = this;
+Paths.prototype._rowConverter = function(d) {
+	return {
+		date: new Date(+d.year, (+d.month - 1)),
+		average: parseFloat(d.average)
+	};
+};
 
-	this.duration = 500;
-	this.ease = d3.easeLinear;
-	this.dataset = this._getDataset('initial');
-	this.key = function(d) {
-		return d.key;
-	}
+Paths.prototype.loadData = function(dataUrl) {
+	d3.csv(dataUrl, this._rowConverter, function(data) {
+		var dataset = data;
 
-	this.xScale = d3.scaleBand()
-		.domain(d3.range(this.dataset.length))
-		.rangeRound([this.padding, this.width - this.padding])
-		.paddingInner(0.05);
-
-	var yScale = d3.scaleLinear()
-		.domain([0, this.maxYValue])
-		.range([this.height - this.padding, this.padding]);
-
-	var svg = d3.select('.svg-container')
-		.append('svg')
-		.attr('class', 'barchart')
-		.attr('width', this.width)
-		.attr('height', this.height);
-
-	svg.selectAll('rect')
-		.data(this.dataset, this.key)
-		.enter()
-		.append('rect')
-		.attr('x', function(d, i) {
-			return _this.xScale(i);
-		})
-		.attr('width', _this.xScale.bandwidth)
-		.attr('y', function(d) {
-			return yScale(d.value);
-		})
-		.attr('height', function(d) {
-			return _this.height - yScale(d.value) - _this.padding;
-		})
-		.attr('class', 'bar')
-		.attr('fill', _this.colour)
-		.on('mouseover', function(d) {
-			var xPosition = parseFloat(d3.select(this).attr('x')) + _this.xScale.bandwidth() / 2;
-			var yPosition = parseFloat(d3.select(this).attr('y')) / 2 + (_this.height / 2);
-			var value = d.value;
-
-			d3.select(this)
-				.transition('hover')
-				.duration(_this.duration / 4)
-				.ease(_this.ease)
-				.attr('fill', _this.colour_hover);
-			d3.select('.tooltip')
-				.classed('is-hidden', false)
-				.style('left', xPosition + 'px')
-				.style('top', yPosition + 'px')
-					.select('.tooltip__value')
-					.text(value);
-		})
-		.on('mouseout', function() {
-			d3.select(this)
-				.transition('hover')
-				.duration(_this.duration)
-				.attr('fill', _this.colour);
-			d3.select('.tooltip')
-				.classed('is-hidden', true);
-		});
-
-	d3.select('#sort')
-		.on('click', function() {
-			_this._sortBars(svg);
-		});
-}
-
-Barchart.prototype._sortBars = function(svg) {
-	var _this = this;
-
-	svg.selectAll('rect')
-		.sort(function(a, b) {
-			return d3.ascending(a.value, b.value);
-		})
-		.transition('sort')
-		.duration(_this.duration)
-		.ease(_this.ease)
-		.attr('x', function(d, i) {
-			return _this.xScale(i);
-		});
-}
-
-Barchart.prototype._getDataset = function(step) {
-	if (step === 'initial') {
-		var dataset = [];
-
-		for (var i = 0, max = this.numValues; i < this.numValues; i++) {
-			var obj = {key: i, value: Math.floor(Math.random() * this.maxYValue)};
-
-			dataset.push(obj);
-		}
-	} else if (step === 'increment') {
-		var dataset = this.dataset;
-		var obj = {key: dataset[dataset.length - 1].key + 1, value: Math.floor(Math.random() * this.maxYValue)};
-
-		dataset.push(obj);
-	}
-
-	return dataset;
-}
+		console.table(dataset, ['date', 'average']);
+	});
+};
--- js/app.js
+++ js/app.js
@@ -1,25 +1,10 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  var numValues = 15;
-  var maxXValue = 500;
-  var maxYValue = 100;
-  var width = 600;
-  var height = 250;
-  var padding = 20;
-  var colour = 'teal';
-  var colour_hover = 'orange';
+  var dataUrl = 'src/mauna_loa_co2_monthly_averages.csv';
+	var paths = new Paths();
 
-	var barchart = new Barchart();
+  paths.loadData(dataUrl);
 
-  barchart.numValues = numValues;
-  barchart.maxXValue = maxXValue;
-  barchart.maxYValue = maxYValue;
-  barchart.width = width;
-  barchart.height = height;
-  barchart.padding = padding;
-  barchart.colour = colour;
-  barchart.colour_hover = colour_hover;
-
-  barchart.draw();
+  console.log(paths);
 }
